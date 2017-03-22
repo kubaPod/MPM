c5f050e1e141557d893e0d6cb9028e671c8a6f89
Kuba Podkalicki <kuba.pod@gmail.com>
2017-03-22 08:30:56 +0100

    init

---
+++ MPM/MPM.m
@@ -0,0 +1,64 @@
+(* ::Package:: *)
+
+BeginPackage["MPM`"];
+
+
+  GitHubPacletInstall::usage = "
+      GitHubPacletInstall[author, pacletName] installs paclet distributed via GitHub repository release
+
+  ";
+
+
+
+Begin["`Private`"];
+
+
+  (*GitHubPacletInstall // Options = {
+    Method \[Rule] Automatic
+  };*)
+
+  GitHubPacletInstall::noass = "Couldn't find assets for the release: ``";
+
+  GitHubPacletInstall[author_String, paclet_String, version_String:"latest"]:=Module[
+    {json, downloads, pacletAssetPattern}
+
+  , pacletAssetPattern = KeyValuePattern["browser_download_url" -> url_String /; StringEndsQ[url, ".paclet"]]:>url
+
+  ; Catch[
+       json = Import[
+         StringTemplate["https://api.github.com/repos/`1`/`2`/releases/`3`"][author, paclet, version]
+       , "RawJSON"
+       ]
+     ; json //Print
+     ; downloads = If[
+         Not @ MatchQ[
+           json
+         , KeyValuePattern["assets" -> _List ? (MemberQ[First @ pacletAssetPattern])]
+         ]
+       , Message[GitHubPacletInstall::noass, version]
+       ; Throw @ $Failed
+       , Cases[json["assets"], pacletAssetPattern ]
+       ]
+
+     ; GitHubPacletInstall /@ downloads
+   ]
+  ]
+
+
+(*      ; target = FileNameJoin[{CreateDirectory[], "paclet.paclet"}]
+   ; If[
+            $Notebooks
+          , PrintTemporary @ Labeled[ProgressIndicator[Appearance -> "Necklace"]
+              , "Downloading...", Right]
+          , Print["Downloading..."]
+        ]
+      ; URLSave[download, target]
+      , Return[$Failed]
+    ]
+  ; If[FileExistsQ[target], PacletInstall[target], $Failed]
+]*)
+
+
+End[];
+
+EndPackage[];
---
+++ MPM/PacletInfo.m
@@ -0,0 +1,15 @@
+
+Paclet[
+    Name -> "MPM",
+    Version -> "0.1.0",
+    MathematicaVersion -> "11+",
+    Description -> "",
+    Creator -> "Jakub (Kuba) Podkalicki",
+    Extensions -> 
+        {
+            {"Kernel", Context -> "MPM`"}
+        }
+]
+
+
+
